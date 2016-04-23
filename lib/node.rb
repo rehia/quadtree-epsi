426e4d313751dc5dbccacf118e8cbec36081df1a
Aschen <amaret93@gmail.com>
2016-04-23 19:11:16 +0200

    Node: #initialize : code
    [52]

--- lib/node.rb
+++ lib/node.rb
@@ -1,13 +1,21 @@
 require 'byebug'
 require 'awesome_print'
+require 'ostruct'
 
 class Node
+
+  X, Y = 0, 1
+  BOUNDARIES = %i(top_left top_right bottom_right bottom_left)
 
   attr_accessor :boundaries, :childrens, :points
 
   def initialize(boundaries = {})
+    check_boundaries_presence(boundaries)
+
     @childrens = Array.new(4) { nil }
     @points = Array.new
+    @boundaries = OpenStruct.new(boundaries)
+
   end
 
   def <<(point)
@@ -20,6 +28,14 @@ class Node
   private # ====================================================================
 
   def subdivide
+  end
+
+  def check_boundaries_presence(boundaries)
+    missings = BOUNDARIES - boundaries.keys
+
+    if missings.any?
+      raise ArgumentError, "You must provide 4 boundaries (#{missings} are missing)"
+    end
   end
 
 end
