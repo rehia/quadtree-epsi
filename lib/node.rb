4246bcc61a28f654707c4d6d78f0b672b2bb53b7
Aschen <amaret93@gmail.com>
2016-04-23 19:29:17 +0200

    Node: add_point : specs
    [32]

--- lib/node.rb
+++ lib/node.rb
@@ -1,13 +1,22 @@
 require 'byebug'
 require 'awesome_print'
 require 'ostruct'
+require 'forwardable'
 
 class Node
+  # Config =====================================================================
+
+  extend Forwardable
 
   X, Y = 0, 1
+  
   BOUNDARIES = %i(top_left top_right bottom_right bottom_left)
 
   attr_accessor :boundaries, :childrens, :points
+
+  def_delegators :@boundaries, *BOUNDARIES
+
+  # Class declaration ==========================================================
 
   def initialize(boundaries = {})
     check_boundaries_presence(boundaries)
@@ -18,12 +27,18 @@ class Node
 
   end
 
-  def <<(point)
+  def add_point(*point)
+    raise ArgumentError, "More than 2 coordinates received" if point.size > 2
+    raise ArgumentError, "Point #{point} is outside boundaries" unless belongs_to_boundaries(point)
+
     points << point
 
-    if points > 4
+    if points.count > 4
     end
+
+    self
   end
+  alias_method :<<, :add_point
 
   private # ====================================================================
 
@@ -36,6 +51,10 @@ class Node
     if missings.any?
       raise ArgumentError, "You must provide 4 boundaries (#{missings} are missing)"
     end
+  end
+
+  def belongs_to_boundaries(point)
+    true
   end
 
 end
--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -25,7 +25,36 @@ RSpec.describe Node do
         }.to raise_error(ArgumentError)
       end
     end
+  end
 
+  describe '#add_point (or <<)' do
+    let(:node) { Node.new(top_left: [0, 0], top_right: [0, 99],
+                         bottom_right: [99, 99], bottom_left: [99, 0]) }
+
+    context 'with more than 2 coordinates' do
+      it 'raise error' do
+        expect {
+          node.add_point(42, 42, 42)
+        }.to raise_error(ArgumentError)
+      end
+    end
+
+    context 'with a point inside boundaries' do
+      it 'add point to the node' do
+        node << [42, 42] << [21, 42]
+        node.add_point(21, 21)
+
+        expect(node.points.count).to eq(3)
+      end
+    end
+
+    context 'with a point outside boundaries' do
+      it 'raise error' do
+        expect {
+          node << [100, 0]
+        }.to raise_error(ArgumentError)
+      end
+    end
   end
 
 end
