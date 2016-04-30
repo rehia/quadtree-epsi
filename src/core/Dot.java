f05ae180abb21cc98851dca88b43c40ebcb930db
jeremiepozzi <jeremie.pozzi@gmail.com>
2016-04-30 09:01:32 +0200

    List of Dots added

---
+++ src/core/Dot.java
@@ -0,0 +1,7 @@
+package core;
+
+/**
+ * Created by J on 30/04/2016.
+ */
+public class Dot {
+}
--- src/core/Grid.java
+++ src/core/Grid.java
@@ -1,10 +1,19 @@
 package core;
 
+import java.util.ArrayList;
+
 /**
  * Created by J on 30/04/2016.
  */
 public class Grid {
+    private ArrayList<Dot> _listOfDots;
+
+    public Grid() {
+        _listOfDots = new ArrayList<>();
+        _listOfDots.add(new Dot());
+    }
+
     public int numberOfDots() {
-        return 1;
+        return _listOfDots.size();
     }
 }
