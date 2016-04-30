6f621f90af881d3ca749f7fc050b1c3165d32828
jeremiepozzi <jeremie.pozzi@gmail.com>
2016-04-30 13:24:22 +0200

    Dot rand isInRange

--- src/core/Dot.java
+++ src/core/Dot.java
@@ -1,11 +1,26 @@
 package core;
 
+import java.util.Random;
+
 /**
  * Created by J on 30/04/2016.
  */
 public class Dot {
 
+    private int _x, _y;
+
+    public Dot() {
+        _x = randInt(0, 100);
+        _y = randInt(0, 100);
+    }
+
+    public static int randInt(int min, int max) {
+        Random rand = new Random();
+        int randomNum = rand.nextInt((max - min) + 1) + min;
+        return randomNum;
+    }
+
     public boolean isIn(int range) {
-        return true;
+        return _x <= range && _y <= range;
     }
 }
