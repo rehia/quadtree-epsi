89577254fd0d4b4bc1cb92baffed8438af75f29d
mnivoliez <mathieu.nivoliez@laposte.net>
2016-05-12 17:15:42 +0200

    The test just pass green. Also little victory: use of private function inside javascript class. A little tricky but interesting. We should now take care of size of container.

--- src/containerNode.js
+++ src/containerNode.js
@@ -2,6 +2,9 @@
 
 const TreeNode = require('./treeNode');
 
+const _addContainer = Symbol('_addContainer');
+const _pushNode = Symbol('_pushNode');
+
 class ContainerNode extends TreeNode {
   constructor() {
     super();
@@ -10,12 +13,27 @@ class ContainerNode extends TreeNode {
 
   addNode(node) {
     node.setParent(this);
-    this.children.push(node);
+    if(this.children.length === 4) {
+      this[_addContainer]();
+    } else {
+      this[_pushNode](node);
+    }
   }
 
   getChildren() {
     return this.children;
   }
+
+  [_addContainer]() {
+    let container = new ContainerNode();
+    container.setParent(this);
+    this[_pushNode](container);
+  }
+
+  [_pushNode] (node) {
+    this.children.push(node);
+  }
 }
+
 
 module.exports = ContainerNode;
