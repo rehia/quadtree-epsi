ab4ab781210061554bd5dbd1f73715abfecabd53
mnivoliez <mathieu.nivoliez@laposte.net>
2016-05-13 17:24:46 +0200

    Add test about coord. Ass expected, this one turn red and didn't worked out.

--- test/treeNode.js
+++ test/treeNode.js
@@ -10,13 +10,23 @@ describe('Test of TreeNode ', function(){
   let testElems = [
     new ContainerNode(),
     new LeafNode()
-  ]
+  ];
 
   testElems.forEach(function(elem) {
     describe('Test of creation of a ' + elem, function(){
 
       it('Should be an instance of TreeNode', function(done){
         expect(elem).to.be.an.instanceof(TreeNode);
+        done();
+      });
+
+      it('Should have coord x and y', function(done){
+        expect(elem).to.have.property('x');
+        expect(elem).to.have.property('y');
+        expect(elem.getX()).to.be.a('Number');
+        expect(elem.getY()).to.be.a('Number');
+        expect(elem.getX()).to.be.above(0);
+        expect(elem.getY()).to.be.above(0);
         done();
       });
 
