fd623ee08ad606d335447538fd0ec0eb0027e57a
rhanbIT <aniel.jeanbaptiste@gmail.com>
2016-05-13 15:23:05 +0200

    #6 Implementing Node prototype setter's and getter's and its tests

--- prototypes/node.js
+++ prototypes/node.js
@@ -1,7 +1,10 @@
 var Coordinate = require('./coordinate');
 function Node (centerCoordinates, halfDimension) {
-  if (!centerCoordinates instanceof Coordinate) {
-    throw new Error("Must be a Coordinate");
+  if (!(centerCoordinates instanceof Coordinate)) {
+    throw new Error("Node must have Coordinate as attribute value");
+  }
+  if (!(halfDimension instanceof Coordinate)) {
+    throw new Error("Node must have Coordinate as attribute value");
   }
   this.centerCoordinates = centerCoordinates;
   this.halfDimension = halfDimension;
@@ -10,12 +13,18 @@ Node.prototype.getCenterCoordinates = function () {
   return this.centerCoordinates;
 };
 Node.prototype.setCenterCoordinates = function (newCenterCoordinates) {
+  if (!(newCenterCoordinates instanceof Coordinate)) {
+    throw new Error("Node must have Coordinate as attribute value");
+  }
   this.centerCoordinates = newCenterCoordinates;
 };
 Node.prototype.getHalfDimension = function () {
   return this.getHalfDimension;
 };
 Node.prototype.setHalfDimension = function (newHalfDimension) {
+  if (!(newHalfDimension instanceof Coordinate)) {
+    throw new Error("Node must have Coordinate as attribute value");
+  }
   this.halfDimension = newHalfDimension;
 }
 module.exports = Node;
--- test/node.js
+++ test/node.js
@@ -3,8 +3,25 @@ var Node = require('../prototypes/node'),
   expect = require('chai').expect;
 var test = describe('node', function() {
   var testNode = new Node(new Coordinate(0,0), new Coordinate(0, 0));
-  it('should be passing', function() {
-    expect(true).to.be.true;
+  it('should throw error when centerCoordinates value is not construct with an instance of Coordinate', function() {
+    expect(function() {
+      new Node(0, new Coordinate(0, 0));
+    }).to.throw("Node must have Coordinate as attribute value");
+  });
+  it('should throw error when centerCoordinates value is not set with an instance of Coordinate', function () {
+    expect(function () {
+      testNode.setCenterCoordinates(0);
+    }).to.throw("Node must have Coordinate as attribute value");
+  });
+  it('should throw error when halfDimension value is not construct with an instance of Coordinate', function() {
+    expect(function() {
+      new Node(new Coordinate(0, 0), 0);
+    }).to.throw("Node must have Coordinate as attribute value");
+  });
+  it('should throw error when halfDimension value is not set with an instance of Coordinate', function () {
+    expect(function () {
+      testNode.setHalfDimension(0);
+    }).to.throw("Node must have Coordinate as attribute value");
   });
 });
 module.exports = test;
