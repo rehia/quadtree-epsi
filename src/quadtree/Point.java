580588a361a7500ddcb0346e2035a6d2c5dad118
root <sebdel03@gmail.com>
2016-05-14 16:40:39 +0200

    Ajout Point random

--- src/quadtree/Point.java
+++ src/quadtree/Point.java
@@ -2,11 +2,16 @@ package quadtree;
 
 public class Point {
 	
-	float x;
+    float x;
     float y;
     
     public Point(float x, float y){
         this.x=x;
         this.y=y;
+    }
+	
+     public Point(int max, int min){
+        this.abcisseX = (int)(Math.random() * (max-min));
+        this.abcisseX = (int)(Math.random() * (max-min));
     }
 }
