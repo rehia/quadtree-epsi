8ba369eb23fb71d929bb255ae7b5b56b4f062e94
Maxime <13Mampalen>
2016-05-15 15:38:30 +0200

    nouvelles fonctions Rectangles et leurs tests

--- quadTreeXp/test/test/RectangleTest.java
+++ quadTreeXp/test/test/RectangleTest.java
@@ -17,12 +17,15 @@ import quadtreexp.Rectangle;
  */
 public class RectangleTest {
     private Rectangle rectangle;
-    private Point point;
+    private Point point, point1, point2;
     int largeur, hauteur;
     
     @Before
     public void setUp() {
         point = new Point(50, 20);
+        point1 = new Point(55,25);
+        point2 = new Point(53,27);
+
         rectangle = new Rectangle(point, 10, 10);
     }
     
@@ -43,6 +46,33 @@ public class RectangleTest {
         assertEquals(true, rectangle.contient(point));
     }
     
-   
+    @Test
+    public void ajoutDePointDansUnRectangle(){
+        rectangle.ajouter(point1);
+        rectangle.ajouter(point2);
+        assertEquals(2, rectangle.getPoints().size(), 0);
+    }
+    
+    @Test
+    public void ajoutDeRectangleDansUnRectangle(){
+        Rectangle rectangle1 = new Rectangle(point1,5,5);
+        rectangle.ajouter(rectangle1);
+        assertEquals(1, rectangle.getRectangles().size(), 0);
+    }
+    
+    @Test
+    public void leRectangleContientUnAutreRectangle(){
+        Rectangle autreRectangle = new Rectangle(point1, 5, 7);
+        rectangle.ajouter(autreRectangle);
+        assertEquals(false, rectangle.estVide());
+    }
+    
+    @Test
+    public void nombreDePoints(){
+        rectangle.ajouter(point1);
+        rectangle.ajouter(point2);
+        assertEquals(2, rectangle.nombreDePoints(), 0);
+    }
+    
 
 }
