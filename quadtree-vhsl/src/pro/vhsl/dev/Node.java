9c0c8d0e9fc3300a9194255250067a407fe7b398
vaimana <vaimana.huet@gmail.com>
2016-05-16 22:22:23 +0200

    TDD Cycle #003 - Refactor

--- quadtree-vhsl/src/pro/vhsl/dev/Node.java
+++ quadtree-vhsl/src/pro/vhsl/dev/Node.java
@@ -1,17 +1,22 @@
 package pro.vhsl.dev;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Node {
 
 	private double x;
 	private double y;
 	private double width;
 	private double height;
+	private List<XY> points;
 
 	public Node(double x, double y, double width, double height) {
 		this.x = x;
 		this.y = y;
 		this.width = width;
 		this.height = height;
+		this.setPoints(new ArrayList<>());
 	}
 
 	public double getX() {
@@ -44,6 +49,24 @@ public class Node {
 
 	public void setHeight(double height) {
 		this.height = height;
+	}
+
+	public List<XY> getPoints() {
+		return points;
+	}
+
+	public void setPoints(List<XY> points) {
+		this.points = points;
+	}
+
+	public void push(XY point) {
+		if(!this.isNotInBound(point))
+			this.getPoints().add(point);
+	}
+
+	private boolean isNotInBound(XY point) {
+		return 	point.getX() < this.x || point.getX() >= this.x + this.width ||
+				point.getY() < this.y || point.getY() >= this.y + this.height;
 	}
 	
 }
