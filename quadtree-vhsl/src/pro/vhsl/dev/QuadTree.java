0f5500fb3daf8d7337104cb0d805bef0d3e70ef4
vaimana <vaimana.huet@gmail.com>
2016-05-17 00:07:46 +0200

    TDD Cycle #008 - Refactor

--- quadtree-vhsl/src/pro/vhsl/dev/QuadTree.java
+++ quadtree-vhsl/src/pro/vhsl/dev/QuadTree.java
@@ -1,11 +1,16 @@
 package pro.vhsl.dev;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuadTree {
 
 	private Node root;
+	private List<XY> points;
 
 	public QuadTree(double width, double height) {
 		this.root = new Node(0, 0, width, height);
+		this.points = new ArrayList<>();
 	}
 
 	public Node getRoot() {
@@ -14,6 +19,37 @@ public class QuadTree {
 
 	public void setRoot(Node root) {
 		this.root = root;
+	}
+
+	public List<XY> getPoints() {
+		return points;
+	}
+
+	public void setPoints(List<XY> points) {
+		this.points = points;
+	}
+
+	public void push(XY point) {
+		if(this.containsPoint(point))
+			return;
+		
+		boolean isInQuadTree = this.root.push(point);
+		
+		if(isInQuadTree)
+			this.points.add(point);
+	}
+
+	private boolean containsPoint(XY point) {
+		boolean result = false;
+		
+		for(XY p : this.points){
+			if(p.getX() == point.getX() && p.getY() == point.getY()){
+				result = true;
+				break;
+			}
+		}
+		
+		return result;
 	}
 
 }
