a001b790e0a5d07be4cbbe3c67d69a08b4117960
mathieu <mathieu@MATHIEU-MSI>
2016-05-16 11:37:33 +0200

    Version 0.0 : Starting to implement Coordinates

--- src/quadtree/epsi/Coordinates.java
+++ src/quadtree/epsi/Coordinates.java
@@ -10,5 +10,32 @@ package quadtree.epsi;
  * @author mathieu
  */
 public class Coordinates {
+    private int X;
+    private int Y;
+
+    public void setX(int X) {
+        if(isValidCoordinate())
+        this.X = X;
+    }
+
+    public void setY(int Y) {
+        this.Y = Y;
+    }
+
+    public int getX() {
+        return X;
+    }
+
+    public int getY() {
+        return Y;
+    }
     
+    public Coordinates(){
+        
+    }
+
+    private boolean isValidCoordinate() {
+       
+    return true;
+    }
 }
