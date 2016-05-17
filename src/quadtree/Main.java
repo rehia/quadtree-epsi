02d23874a4df089b0af5e56fd9a7ff2c97fbfbdc
sebastiendelots <sebdel03@gmail.com>
2016-05-17 23:13:47 +0200

    Update Main.java

--- src/quadtree/Main.java
+++ src/quadtree/Main.java
@@ -1,10 +1,19 @@
 package quadtree;
 
+import Quadtree.java;
+import Point.java;
+
 public class Main {
 
 	public static void main (String [] arg)
 	{
-		System.out.println("bonjour");
+		private final int nbPointMax = 4;
+		private ArrayList<Point>ListeDePoint;
+		
+		Quadtree quadtree = new Quadtree();
+		Point point = new Point(100,0,true);
+		quadtree.insertUniquePoint(point);
+		
 	}
 	
 }
