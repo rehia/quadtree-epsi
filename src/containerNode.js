8f438bf9de2aac0232b2c75809d3898dd41ed1c1
Mathieu Nivoliez <mathieu.nivoliez@laposte.net>
2016-05-17 19:26:10 +0200

    Refactor done.  Also, came to my mind that we don't test if a container continue to create subcontainer if we had an other point after a first division. Let's write a test!

--- src/containerNode.js
+++ src/containerNode.js
@@ -4,6 +4,8 @@ const TreeNode = require('./treeNode');
 
 const _divideInFourContainer = Symbol('_divideInFourContainer');
 const _calculateFutureCoordonate = Symbol('_calculateFutureCoordonate');
+const _addTheFourContainerNode = Symbol('_addTheFourContainerNode');
+const _createContainerNode = Symbol('_createContainerNode');
 const _pushNode = Symbol('_pushNode');
 
 class ContainerNode extends TreeNode {
@@ -35,17 +37,26 @@ class ContainerNode extends TreeNode {
   }
 
   [_divideInFourContainer](){
+    this[_addTheFourContainerNode]();
+  }
+
+  [_addTheFourContainerNode]() {
     let sideSize = this.sideSize / 2;
     for(let x=0; x < 2; x++) {
       for(let y=0; y < 2; y++) {
         let futureX = this[_calculateFutureCoordonate](this.x, x===0 ? 1:sideSize);
         let futureY = this[_calculateFutureCoordonate](this.y, y===0 ? 1:sideSize);
-        let container = new ContainerNode(futureX, futureY);
-        container.setSideSize(sideSize);
-        container.setParent(this);
+        let container = this[_createContainerNode](futureX, futureY, sideSize);
         this[_pushNode](container);
       }
     }
+  }
+
+  [_createContainerNode](x, y, sideSize) {
+    let container = new ContainerNode(x,y);
+    container.setSideSize(sideSize);
+    container.setParent(this);
+    return container;
   }
 
   [_calculateFutureCoordonate](baseCoord, offset = 1) {
@@ -55,7 +66,6 @@ class ContainerNode extends TreeNode {
   [_pushNode] (node) {
     this.children.push(node);
   }
-
 }
 
 
