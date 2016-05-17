6ad51b17fe7945b7761b7bb60343db1c585e6210
maelprothon <mael.prothon@epsi.fr>
2016-05-17 22:57:35 +0200

    main.java
    A completer avec les appels de méthodes qui doivent retournés des valeurs avec System.out.println().

---
+++ Main.java
@@ -0,0 +1,29 @@
+package quadtree;
+
+public class Main {
+
+  	public static void main(String[] args){
+       String action = args[0];
+       String X_point = args[1];
+       String Y_point = args[2];
+      
+       if(action == list)
+       {
+
+        //   appel de la methode qui liste les points générés aléatoirement
+
+       }
+       else if(action == depth)
+       {
+        
+        //   appel de la methode qui donne la profondeur du point de coordonée X_point,Y_point
+ 
+       }
+       else if(action == nearest)
+       {
+
+        //    appel de la methode qui donne les points les plus proches du point de coordonées X_point,Y_point
+  
+       }
+    }
+}
