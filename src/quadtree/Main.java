0b8c716e32e8395781e56f0409442cf924e756f2
Delpino <dorian.delpino@voila.fr>
2016-05-18 22:31:14 +0200

    Commit Modif du main

--- src/quadtree/Main.java
+++ src/quadtree/Main.java
@@ -9,11 +9,32 @@ public class Main {
 	private ArrayList<Point>ListeDePoint;
 
   	public static void main(String[] args){
-  	  
-       //String action = args[0];
-  		String action = "depth";
-      // String X_point = args[1];
-       //String Y_point = args[2];
+  	  String action ="";
+  	  String X_point="";
+  	  String Y_point="";
+	   if(args.length >0)
+	   {
+		   if(args[0].length() != 0)
+	       {
+	    	    action = args[0];
+	       }
+	       if(args[1].length() != 0)
+	       {
+	    	   	X_point = args[1];
+	       }
+	       if(args[1].length() != 0)
+	       {
+	    	   	Y_point = args[2];
+	       }
+	       
+	   }
+	   else
+	   {
+		   action = "list";
+	   }
+	  		
+       
+       //String action = "list";
        
        final int nbPointMax = 4;
 		
