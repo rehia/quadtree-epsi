bb06def42d253d2693044956697fd4e5916b0757
maelprothon <mael.prothon@epsi.fr>
2016-05-20 22:05:00 +0200

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -4,29 +4,13 @@ MYSELF=$(which "$0" 2>/dev/null)
 
 [ $? -gt 0 -a -f "$0" ] && MYSELF="./$0"
 
-JAVA_OPT=""
-INVALID_OPT="Invalid option :"
 HELP="HELP: Quadtree Java Programme :
 
-1) ./appquadtree -list
-2) ./appquadtree -depth 10 20     
-3) ./appquadtree -nearest 10 20
-
--list | -l : List the all generated points.
-
--depth | -d : Know the depth of a point in the tree , with the X,Y coordinates of the point.
-
--nearest | -n : Know the closest to a given point points, with the X,Y coordinates of the point
+1) ./appquadtree 
+2) Enter the coordinate of X and Y of the requested point and what you want from this point ( depth and / or neighbors) .
+3) Enjoy it !!
 "
+if [[ $1 == "HELP"]]
+echo "$HELP" ;;
 
-case $1 in
-   -list|-l)   JAVA_OPT="list" ;;
-   -depth|-d)   JAVA_OPT="depth $2 $3" ;;
-   -nearest|-n)   JAVA_OPT="nearest $2 $3" ;;
-   -help|-h)   echo "$HELP" ;;
-   \?)   echo "$INVALID_OPT $1" ;;
-esac
-
-if test -n "$JAVA_OPT" ; then
-     exec java -jar $MYSELF $JAVA_OPT
-fi
+exec java -jar $MYSELF
